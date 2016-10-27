16688657d1e5f1ef3d8b57175cdd87f51c83af58
Alec Friedman <alec.friedman3@gmail.com>
2016-10-27 17:29:15 -0400

    test specs for reductions

--- reductions/reduce.js
+++ reductions/reduce.js
@@ -2,12 +2,12 @@
 // countChange takes a value, and an array of coins, and finds the total number of combinations it takes to make change for the value with your given coin values
 var knownResults = {};
 
-var countChange = function(money, coins) {
+const countChangeCombinations = function(money, coins) {
   let mem = 'money: '+ money +'coins: ' + coins.join('');
   if (knownResults[mem]) return knownResults[mem];
   if (money < 0 || !coins.length) return 0;
   if (money === 0) return 1;
-  return knownResults[mem] = countChange(money - coins[0], coins) + countChange(money, coins.slice(1))
+  return knownResults[mem] = countChangeCombinations(money - coins[0], coins) + countChangeCombinations(money, coins.slice(1))
 }
 
 
@@ -17,13 +17,13 @@ var countChange = function(money, coins) {
 
 // 4, 3 + 1, 2 + 2, 2 + 1 + 1, 1 + 1 + 1 + 1.
 
-// write a helper function which `reduces` the partitionsOfNum problem into some instance of the coinChange problem. Our partitions problem is really just a special case of countChange
+// write a helper function which `reduces` the partitionsOfNum problem into some instance of the coinChange problem. Our partitions problem is really just a special case of countChangeCombinations
 
-const partitionsOfNum = function (num) {
+const combinatorialPartitonsReduceToCoins = function (num) {
   function changeReduction (n) {
     return new Array(n).fill(0).map((e, i) => i + 1)
   }
-  return countChange(num, changeReduction(num))
+  return countChangeCombinations(num, changeReduction(num))
 }
 
 
@@ -39,27 +39,57 @@ const knapsackProblem = function (set, target) {
     for (var i = 0; i < set.length; i++){
       let now = set[i];
       let next = set.slice(0, i).concat(set.slice(i + 1));
-      if (subsetSum(next, snoball + now)) return true
+      if (subset(next, snoball + now)) return true
     }
     return false
   }
   return subset(set, 0)
 }
 
-const subsetSum = function (set) {
+const subsetSumReduceToKnapsack = function (set) {
   return knapsackProblem(set, 0)
 }
 
-const partitionProblem (set) {
+const partitionProblemReducedToKnapsack = function (set) {
   // can we find two subsets of our initial set of numbers, such that the sums of those 2 subsets sums to 0?
-  // can we reduce this to our subset sum problem above?
+  // Can we reduce partitions to knapsack? Can we reduce in the other direction??
+  return knapsackProblem(set, set.reduce((a, b) => a + b) / 2)
+}
+
+const partitionProblem = function (set) {
+  function compare (arr, sum){
+    return arr.reduce((a , b) => a + b) === sum;
+  }
+
+  function partition (arr, snowball) {
+    if (!arr.length){
+      return false
+    }
+    if (compare(arr, snowball) && arr.length !== set.length) {
+      return true;
+    }
+    for (let i = 0; i < arr.length; i++) {
+      let now = arr[i];
+      let next = arr.slice(0, i).concat(arr.slice(i + 1));
+      if (partition(next, snowball + now)) return true
+    }
+    return false
+  }
+
+  return partition(set, 0)
+}
+
+const knapsackReduceToPartiton = function (set, target) {
+  return partitionProblem(set.concat(target * 2 + set.reduce((a, b) => a + b)))
 }
 
 
-
 module.exports = {
-  countChange,
-  partitionsOfNum,
-  subsetSum,
-  knapsackProblem
+  countChangeCombinations,
+  combinatorialPartitonsReduceToCoins,
+  knapsackProblem,
+  subsetSumReduceToKnapsack,
+  partitionProblemReducedToKnapsack,
+  partitionProblem,
+  knapsackReduceToPartiton
 }
--- reductions/reduce.test.js
+++ reductions/reduce.test.js
@@ -1,15 +1,141 @@
-const expect = require('chai').expect;
-// const algorithms = require('./algorithms')
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon')
+// const should = chai.should();
+// const spies = require('chai-spies');
+// chai.use(spies);
 
-describe('The Algorithm', () => {
+const methods = require('./experimenting');
+// const {
+//   countChangeCombinations,
+//   combinatorialPartitonsReduceToCoins,
+//   knapsackProblem,
+//   subsetSumReduceToKnapsack,
+//   partitionProblemReducedToKnapsack,
+//   partitionProblem,
+//   knapsackReduceToPartiton
+// } = methods
 
-  describe('for searching an array', () =>{
+describe('Reductions:', () => {
+  let knapsackSpy, coinSpy, partitionSpy;
 
-    it ('should return an array', () => {
-      // expect(algorithms.search()).to.be.an('array')
+  describe('The Counting Change combinations problem', () =>{
+
+    it ('should count the number of ways to make change from a given list of coins', () => {
+      expect(methods.countChangeCombinations(4, [1, 2])).to.be.equal(3);
+      expect(methods.countChangeCombinations(10, [5, 2, 3])).to.be.equal(4);
+      expect(methods.countChangeCombinations(11, [5, 7])).to.be.equal(0);
+      expect(methods.countChangeCombinations(61, [1, 5, 10, 25])).to.be.equal(73);
+      expect(methods.countChangeCombinations(572, [8, 12, 13, 94, 27])).to.be.equal(2317)
     })
 
-    it ('should test', () => {
+    // CAUTION THIS WILL TAKE A LONG TIME TO RUN IF YOU DO NOT USE MEMOIZATION
+    it ('only try this if you think your algorithm is efficient!', function() {
+      this.timeout(5000)
+      expect(methods.countChangeCombinations(363, [2, 7, 4, 8, 16])).to.be.equal(118519)
+    })
+
+  })
+
+  describe('The Partitions of an integer', () => {
+
+    it ('should reduce to Counting Change (by calling countChangeCombinations)', () => {
+      coinSpy = sinon.spy(methods, 'countChangeCombinations');
+      methods.combinatorialPartitonsReduceToCoins(5);
+      expect(coinSpy.called).to.be.true;
+      methods.countChangeCombinations.restore();
+    })
+
+    it ('should count the number of unique partitions of an integer n', () => {
+
+      expect(methods.combinatorialPartitonsReduceToCoins(5)).to.be.equal(7)
+      expect(methods.combinatorialPartitonsReduceToCoins(6)).to.be.equal(11)
+      expect(methods.combinatorialPartitonsReduceToCoins(7)).to.be.equal(15)
+      expect(methods.combinatorialPartitonsReduceToCoins(9)).to.be.equal(30)
+      expect(methods.combinatorialPartitonsReduceToCoins(11)).to.be.equal(56)
+      expect(methods.combinatorialPartitonsReduceToCoins(24)).to.be.equal(1575)
+
+    })
+
+
+  })
+
+  describe('The Knapsack Problem', () => {
+
+    it ('should determine whether a set of numbers contains a subset which sums to a given value', () => {
+
+      expect(methods.knapsackProblem([1, 3, 6, 7, 9], 12)).to.be.true;
+      expect(methods.knapsackProblem([2, 4, 7, 11], 13)).to.be.true;
+      expect(methods.knapsackProblem([2, 3, 8, 16], 9)).to.be.false;
+
+    })
+
+  })
+
+  describe('The Subset Sum Problem reduction', () => {
+
+    it ('should reduce to the knapsackProblem', () => {
+      knapsackSpy = sinon.spy(methods, 'knapsackProblem');
+      methods.subsetSumReduceToKnapsack([1, 3, -2, -3, 4, 5]);
+      expect(knapsackSpy.called).to.be.true;
+      methods.knapsackProblem.restore();
+    })
+
+    it ('should determine whether a set of numbers contains a subset summing to 0', () => {
+
+      expect(methods.subsetSumReduceToKnapsack([1, 2, 3, 4, 5])).to.be.false
+      expect(methods.subsetSumReduceToKnapsack([1, 3, -2, -3, 4, 5])).to.be.true
+
+    })
+
+  })
+
+  describe('The Set Partition Problem reduction to Knapsack', () => {
+
+    it ('should reduce to the knapsackProblem', () => {
+      knapsackSpy = sinon.spy(methods, 'knapsackProblem');
+      methods.partitionProblemReducedToKnapsack([1, 3, -2, -3, 4, 5]);
+      expect(knapsackSpy.called).to.be.true;
+      methods.knapsackProblem.restore();
+    })
+
+    it ('should determine whether a set of numbers contains two subsets which sum to the same value', () => {
+      expect(methods.partitionProblemReducedToKnapsack([1, 4, 5, 6, 9])).to.be.false
+      expect(methods.partitionProblemReducedToKnapsack([1, 4, 5, 6, 9, 2, 3])).to.be.true
+    })
+
+  })
+
+  describe('The Set Partition Problem without reduction', () => {
+
+    it ('should NOT reduce to the knapsackProblem', () => {
+      knapsackSpy = sinon.spy(methods, 'knapsackProblem');
+      methods.partitionProblem([1, 3, -2, -3, 4, 5]);
+      expect(knapsackSpy.called).to.be.false;
+      methods.knapsackProblem.restore();
+    })
+
+    it ('should determine whether a set of numbers contains two subsets which sum to the same value', () => {
+      expect(methods.partitionProblemReducedToKnapsack([1, 4, 5, 6, 9])).to.be.false
+      expect(methods.partitionProblemReducedToKnapsack([1, 4, 5, 6, 9, 2, 3])).to.be.true
+    })
+
+  })
+
+  describe('The Knapsack Problem reduction to Set Partition', () => {
+
+    it ('should reduce to the Set Partition', () => {
+      partitionSpy = sinon.spy(methods, 'partitionProblem');
+      methods.knapsackReduceToPartiton([1, 3, 6, 7, 9], 12);
+      expect(partitionSpy.called).to.be.true;
+      methods.partitionProblem.restore();
+    })
+
+    it ('should determine whether a set of numbers contains a subset which sums to a given value', () => {
+
+      expect(methods.knapsackReduceToPartiton([1, 3, 6, 7, 9], 12)).to.be.true;
+      expect(methods.knapsackReduceToPartiton([2, 4, 7, 11], 13)).to.be.true;
+      expect(methods.knapsackReduceToPartiton([2, 3, 8, 16], 9)).to.be.false;
 
     })
 
